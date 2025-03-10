d989ebfda1cf6aa1a92122718cc0dd0fd80bbfcb
ThaylanFonseca <thaylanfonseca12@gmail.com>
2025-03-10 18:56:17 -0300

    Add files via upload

---
+++ clone pokemon/src/js/index.js
@@ -0,0 +1,50 @@
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+
+alert("Bem-vindo(a), que tal conhecer alguns pokémons?");
+
+cartoes.forEach(cartao => {
+    cartao.addEventListener("click", function () {
+        const cartaVirada = cartao.querySelector(".carta-virada");
+
+        cartao.classList.toggle("virar");
+
+        cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+        const descricao = cartao.querySelector(".descricao");
+        descricao.classList.toggle("esconder");
+    })
+});
+
+btnAvancar.addEventListener("click", function () {
+    if(cartaoAtual === cartoes.length - 1) {
+        return;
+    }
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual++;
+    mostrarCartao(cartaoAtual)
+});
+
+btnVoltar.addEventListener("click", function(){
+    if(cartaoAtual === 0){
+        return;
+    }
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual--;
+    mostrarCartao(cartaoAtual);
+});
+
+function mostrarCartao(cartaoAtual){
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado(){
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+}
